dc82d52f63c0afc7bc96417da01ca268af0b55d5
Max <maxthomaz2006@gmail.com>
2026-04-16 16:31:04 -0300

    commitando correcao no botao de caso ja tenha conta

--- MesclaInvest/lib/widgets/cadastro_widgets.dart
+++ MesclaInvest/lib/widgets/cadastro_widgets.dart
@@ -204,7 +204,9 @@ class CadastroFooter extends StatelessWidget {
         const Text('Já possui conta? ',
             style: TextStyle(fontSize: 12, color: Colors.black)),
         GestureDetector(
-          onTap: () {},
+          onTap: () {
+            Navigator.of(context).pushReplacementNamed('/login');
+          },
           child: const Text('Entrar',
               style: TextStyle(
                   fontSize: 12,

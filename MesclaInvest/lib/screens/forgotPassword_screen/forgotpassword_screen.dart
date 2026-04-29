a8f68af15281b56c3f5634383d5a687f4f509d9b
Max <maxthomaz2006@gmail.com>
2026-04-29 16:53:24 -0300

    commitando finalizacao de tela de esqueceu senha

---
+++ MesclaInvest/lib/screens/forgotPassword_screen/forgotpassword_screen.dart
@@ -0,0 +1,19 @@
+// Max Thomazini Barbosa RA: 25003934
+import 'package:flutter/material.dart';
+
+// Importa a função que envia email de resetar senha.
+import '../../services/forgotpassword_auth.dart';
+// Importa os widgets reutilizáveis da tela de esqueceu senha.
+import '../../widgets/forgotpassword_widgets.dart';
+// Liga este arquivo ao state separado.
+part 'forgotpassword_screen_state.dart';
+
+// Define a tela de login como um widget com estado.
+class ForgotPasswordScreen extends StatefulWidget {
+  // Cria a tela de login.
+  const ForgotPasswordScreen({super.key});
+
+  // Informa qual classe controla o estado da tela.
+  @override
+  State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
+}

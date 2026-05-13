a2dac96990e4e3071acd9c049483b33724e18957
maxthba <maxthomaz2006@gmail.com>
2026-05-13 18:43:53 -0300

    feat(token): funcao de listar tokens comprados pelo usuario

--- MesclaInvest/functions/lib/exchange/index.js
+++ MesclaInvest/functions/lib/exchange/index.js
@@ -1,10 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.sellTokensHandler = exports.buyTokensHandler = exports.addDepositHandler = exports.getUserBalanceHandler = exports.getUserBalance = void 0;
+exports.sellTokensHandler = exports.buyTokensHandler = exports.addDepositHandler = exports.getUserTokensHandler = exports.getUserBalanceHandler = exports.getUserBalance = void 0;
 var userRepository_1 = require("./repositories/userRepository");
 Object.defineProperty(exports, "getUserBalance", { enumerable: true, get: function () { return userRepository_1.getUserBalance; } });
 var getUserBalance_1 = require("./handlers/getUserBalance");
 Object.defineProperty(exports, "getUserBalanceHandler", { enumerable: true, get: function () { return getUserBalance_1.getUserBalanceHandler; } });
+var getUserTokens_1 = require("./handlers/getUserTokens");
+Object.defineProperty(exports, "getUserTokensHandler", { enumerable: true, get: function () { return getUserTokens_1.getUserTokensHandler; } });
 var addDeposit_1 = require("./handlers/addDeposit");
 Object.defineProperty(exports, "addDepositHandler", { enumerable: true, get: function () { return addDeposit_1.addDepositHandler; } });
 var buyTokens_1 = require("./handlers/buyTokens");

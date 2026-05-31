7d599ca63efed3f4e8413cea681d9383a757aa46
maxthba <maxthomaz2006@gmail.com>
2026-05-31 10:55:17 -0300

    commitando correcao de import nao usado em profile_screen_state

--- MesclaInvest/lib/screens/profile_screen/profile_screen.dart
+++ MesclaInvest/lib/screens/profile_screen/profile_screen.dart
@@ -1,5 +1,4 @@
 //Max Thomazini Barbosa RA:25003934
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import '../../widgets/app_bottom_navigation.dart';
